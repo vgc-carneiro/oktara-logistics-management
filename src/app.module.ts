d29f49f36c55d88512d6c83d46cbfa7ad9d4694d
Vinicius Carneiro <vinicius.carneiro@tmov.com.br>
2022-11-12 01:05:23 -0300

    Adding postgreSQL connection with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseConfiguration } from './infrastructure/database/database.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({ useClass: DatabaseConfiguration })],
   controllers: [AppController],
   providers: [AppService],
 })
