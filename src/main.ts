54c4f123446be4436694c784904d23a482139b1c
Vinicius Carneiro <vinicius.carneiro@tmov.com.br>
2022-11-12 00:50:30 -0300

    Adding swagger to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Logistics Management')
+    .setDescription('The Logistics API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3335);
 }
 bootstrap();
